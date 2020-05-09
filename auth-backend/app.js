e24160cd28e0a3aad88b7c0c0d6eb088f682781f
Rakib Khan <rakibkhan@live.co.uk>
2020-05-10 00:37:27 +0100

    .

---
+++ auth-backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+const cors = require('cors');
+
+const app = express();
+
+mongoose.connect('mongodb://authUser:authPass123@ds045948.mlab.com:45948/auth_test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log('connected to db'))
+    .catch(err => console.log(err))
+
+// * Middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+
+// * Route middleware
+// app.use('/api', authRoutes);
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, console.log('started listening'));
